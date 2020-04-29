baaaba7e9c66100615814e764b51cf7f13833ade
ulagi21 <63046100+ulagi21@users.noreply.github.com>
2020-04-29 22:25:46 +0900

    first js

---
+++ index.js
@@ -0,0 +1,25 @@
+const monday = "Mon";
+const tue = "Tue";
+const wed = "Wed";
+const thu = "Thu";
+const fri = "Fri";
+
+console.log(monday, tue, wed, thu, fri);
+
+const ryuInfo = ["Ryu", "35", true, "Seoul"];
+
+console.log(ryuInfo);
+
+const ikhyunInfo = {
+  name: "Ikhyun",
+  age: 35,
+  gender: "Male",
+  isHandsome: true,
+  favMovies: ["Sound of Music", "Love Acually"],
+  favFood: [
+    { first: "agg", fatty: false },
+    { secend: "kimghi", fatty: false },
+  ],
+};
+
+console.log(ikhyunInfo.favFood[1].fatty);
